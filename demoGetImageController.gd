7b8e5760d5a676ee2954ab3b72e79d8ced6ae8f7
ZaidDev <44425998+ZaidProjectsDev@users.noreply.github.com>
2024-04-10 12:41:15 +0200

    Started User Story

---
+++ demoGetImageController.gd
@@ -0,0 +1,34 @@
+extends Node2D
+var imagePlugin
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Engine.has_singleton("GodotGetImage"):
+		imagePlugin = Engine.get_singleton("GodotGetImage")
+		imagePlugin.connect("image_request_completed",_set_image)
+
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _get_gallery_selection():
+	if imagePlugin:
+		imagePlugin.GetGalleryImage()
+	else: 
+		print("Button Worked but plugin inst loaded...")
+	pass # Replace with function body.
+
+func _set_image(dict):
+	for image in dict.values():
+		var currentImage = Image.new()
+		currentImage.load_jpg_from_buffer(image)
+		print("Loading Image")
+		await get_tree().process_frame
+		var texture = ImageTexture.new()
+		texture.create_from_image(currentImage)
+		$TextureRect.texture = texture
+	pass
