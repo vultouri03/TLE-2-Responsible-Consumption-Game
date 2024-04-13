acfabb8b35ce82fccdf32ab8cb3d1de5f74484e6
CesianaHZ <davidj.poels@gmail.com>
2024-04-13 14:37:18 +0200

    Base_functionality

---
+++ HTTPRequest.gd
@@ -0,0 +1,27 @@
+extends HTTPRequest
+
+var file = file.new()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_sendToServer()
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _sendToServer():
+
+	var headers = {
+		"Content-Type": "application/json",
+	}
+	var _base_64_data = Marshalls.raw_to_base64(file.get_buffer(file.get_len()))
+	var json = JSON.stringify(_base_64_data)
+
+	var url = ''
+
+	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
+
