ec634adc196ee08acf0153c9f98122c23c5dbd45
CesianaHZ <davidj.poels@gmail.com>
2024-04-20 14:49:42 +0200

    Update HTTPRequest.gd

--- HTTPRequest.gd
+++ HTTPRequest.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var file_path = './Images/Banana.jpg'
-var url = Globalvars.url
+var url = Globalvars.webserver
 enum STATUS {SUCCESS, CLIENT_ERR, SERVER_ERR, NONE, FUNC_ERR}
 
 var requestStatus = STATUS.NONE
@@ -34,12 +34,35 @@ func _convert_to_base64():
 
 func _sendToServer(headers, json):
 
-	_convert_to_base64()
+	var endPointUrl = url + '/classify'
 
 	$HTTPRequest.request_completed.connect(_handleResponse)
 
-	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
+	$HTTPRequest.request(endPointUrl, headers, HTTPClient.METHOD_POST, json)
+	print("Requesting : "+ endPointUrl)
 
+	var error = $HTTPRequest.request(endPointUrl, headers, HTTPClient.METHOD_POST, JSON.stringify(json))
+	
+	if error != OK:
+			print(_get_request_status(STATUS.FUNC_ERR))
+	else:
+		pass
+	pass
+
+
+
+func _get_request_status(status):
+	match status:
+		STATUS.NONE:
+			return "Request not yet executed";
+		STATUS.SUCCESS:
+			return "Request successful";
+		STATUS.CLIENT_ERR:
+			return "Request failed, check your connection."
+		STATUS.SERVER_ERR: 
+			return "Request failed, service is encountering problems. Please be patient."
+		STATUS.FUNC_ERR: 
+			return "There's a software issue."
 
 func _handleResponse(result, response_code, headers, body):
 	match response_code:
@@ -49,6 +72,15 @@ func _handleResponse(result, response_code, headers, body):
 			requestStatus = STATUS.CLIENT_ERR
 		500:
 			print('Internal Server Error')
+	
+	print(headers)
+	print(result)
+	print(response_code)
+
+	print(_get_request_status(requestStatus))
+
+	var responseBody = JSON.parse_string(body.get_string_from_utf8())
+	print(responseBody)
 		
 	
 
