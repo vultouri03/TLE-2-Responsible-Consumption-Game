2015ea534a721e5eb848466d563fc59559e7ce8c
CesianaHZ <davidj.poels@gmail.com>
2024-04-20 14:27:26 +0200

    working on bug fixes

--- HTTPRequest.gd
+++ HTTPRequest.gd
@@ -1,10 +1,14 @@
-extends HTTPRequest
+extends Node
 
 var file_path = './Images/Banana.jpg'
 
+enum STATUS {SUCCESS, CLIENT_ERR, SERVER_ERR, NONE, FUNC_ERR}
+
+var requestStatus = STATUS.NONE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_sendToServer()
+	_convert_to_base64()
 	print('Ready')
 
 
@@ -12,8 +16,7 @@ func _ready():
 func _process(delta):
 	print('Processing')
 
-func _sendToServer():
-
+func _convert_to_base64():
 	var image = Image.new()
 	image.load(file_path)
 	var image_data = image.save_jpg_to_buffer()
@@ -27,20 +30,41 @@ func _sendToServer():
 
 	print(json)
 
-	var url = 'http://localhost:8000/classify'
+	_sendToServer(headers, json)
 
-	self.request(url, headers, HTTPClient.METHOD_POST, json)
+func _sendToServer(headers, json):
 
-	var response = json.parse($HTTPRequest.get_response_data())
+	_convert_to_base64()
 
-	if $HTTPRequest.get_response_code() == 200:
-		_handleResponse('SUCCESS ' + response)
-	else:
-		_handleResponse('FAILED ' + response)
+	var url = 'https://tle-2-receipt-scan-webservice.onrender.com/'
 
+	$HTTPRequest.request_completed.connect(_handleResponse)
 
-func _handleResponse(response):
-	print(response)
+	$HTTPRequest.request(url, headers, HTTPClient.METHOD_POST, json)
+
+func _get_request_status(status):
+	match status:
+		STATUS.NONE:
+			return "Request not yet executed";
+		STATUS.SUCCESS:
+			return "Request successful";
+		STATUS.CLIENT_ERR:
+			return "Request failed, check your connection."
+		STATUS.SERVER_ERR: 
+			return "Request failed, service is encountering problems. Please be patient."
+		STATUS.FUNC_ERR: 
+			return "There's a software issue."
+
+func _handleResponse(result, response_code, headers, body):
+	match response_code:
+		200:
+			requestStatus
+		404:
+			print('Not Found')
+		500:
+			print('Internal Server Error')
+		
+	var json = JSON.parse_string(json.get_string_from_utf8())
 	
 
 	
