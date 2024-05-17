debf3efd99b8d056534714803845593e3a464aec
CesianaHZ <davidj.poels@gmail.com>
2024-05-17 15:19:46 +0200

    Added a button to test

--- inventory.gd
+++ inventory.gd
@@ -1,18 +1,26 @@
 extends Control
 
 var categories = null;
-@export var category_rows = []
-
+var category_names = ["Bread", "Meat", "Drinks", "Spices", "Dairy", "Vegetables", "Sauces"]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
 	categories = load("res://Resources/categories.tres")
+
+	for c in self.get_children():
+		for n in category_names:			
+			if c.get_name().find(n):
+				c.get_node("Amount").text = categories[n]
+				print(c.get_name())
+			else:
+				print("Not a category")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func _grab_children():
-	for category in category_rows:
-		#category.get_node("Amount").text = #categories[category.get_node("amount").text]
+
+func _on_button_pressed():
+	print("Button Pressed")
+	
