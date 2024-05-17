4957c9bf1f092f63f90357446973935e2b6ea024
CesianaHZ <davidj.poels@gmail.com>
2024-05-17 12:43:41 +0200

    Started working on inventory ui

---
+++ inventory.gd
@@ -0,0 +1,18 @@
+extends Control
+
+var categories = null;
+@export var category_rows = []
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	categories = load("res://Resources/categories.tres")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _grab_children():
+	for category in category_rows:
+		category.get_node("Amount").text = #categories[category.get_node("amount").text]
