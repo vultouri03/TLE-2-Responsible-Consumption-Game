1321046ea8217d738788fd7f8e3dac111fbc2160
ZaidDev <44425998+ZaidProjectsDev@users.noreply.github.com>
2024-05-18 15:53:10 +0200

    Finished Menu,

--- Levels/Scr_MainMenu.gd
+++ Levels/Scr_MainMenu.gd
@@ -6,15 +6,15 @@ func _ready():
 	pass # Replace with function body.
 
 var imagePlugin
-
+var currentMenu = "start"
+@export var getImageController:Node2D
 # Get the permissions for the camera.
 func _getImagePermissions():
 	if Engine.has_singleton("GodotGetImage"):
 		imagePlugin = Engine.get_singleton("GodotGetImage")
-		imagePlugin.connect("image_request_completed",_updateCameraButtonState)
-		
 	if imagePlugin:
 		imagePlugin.resendPermission()
+		imagePlugin.getCameraImage()
 	else: 
 		print("Button Worked but plugin inst loaded...")
 	pass # Replace with function body.
@@ -29,6 +29,8 @@ func _process(delta):
 
 func _on_start_pressed():
 	print("Start button pressed")
+	_switch_menu_state()
+	
 	pass # Replace with function body.
 
 
@@ -37,3 +39,32 @@ func _on_camera_pressed():
 	_getImagePermissions()
 	
 	pass # Replace with function body.
+
+
+func _on_back_pressed():
+	_switch_menu_state()
+	pass # Replace with function body.
+
+
+func _on_continue_pressed():
+	get_tree().change_scene_to_file("res://Levels/lvl_main_game.tscn")
+	pass # Replace with function body.
+
+
+func _on_scan_pressed():
+	getImageController._get_camera_image()
+	pass # Replace with function body.
+
+
+func _switch_menu_state():
+	if currentMenu == "start":
+		currentMenu = "sub_menu_1"
+	else:
+		currentMenu = "start"
+	
+	if currentMenu == "start":
+		$VboxStart.visible = true
+		$VBoxSubMenu.visible = false
+	else:
+		$VboxStart.visible = false
+		$VBoxSubMenu.visible = true
