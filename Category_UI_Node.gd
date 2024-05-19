35fb0d0318fa60469da963d1a9f6468a3e9abd68
CesianaHZ <davidj.poels@gmail.com>
2024-05-19 13:53:34 +0200

    Update Category_UI_Node.gd

--- Category_UI_Node.gd
+++ Category_UI_Node.gd
@@ -1,19 +1,30 @@
 extends Sprite2D
 
-@export var item = Area2D
-
 func _on_button_pressed():
+	 
+	#Decrease the amount of items in the inventory
 	var _amountText = self.get_node("Amount").text
-	var _amount = int(_amountText) -1
-	var _amountString = str(_amount)
-	self.get_node("Amount").text = _amountString
+	var _amount = int(_amountText)
+
+	if _amount > 0:
+
+		var _amountString = str(_amount - 1)
+
+		self.get_node("Amount").text = _amountString
+
+		_instantiateDraggable()
+	
+
+
+func _instantiateDraggable():
 
 	#Instantiate draggable item with the right image
 	var scene = preload("res://Scripts/GUI/Draggable.tscn")
 	var instance = scene.instantiate()
+	var textureToChange = instance.get_child(0).texture
+
 	add_child(instance)
-    instance.get_child(0).texture = self.texture
-    instance.get_child(0).frame = self.frame
-    #This currently spawns the full image, but the image is animated and should spawn the right frame based on the one assigned on the parent
 
-
+	textureToChange = self.texture
+	textureToChange.texture.frame = self.texture.frame
+	#This currently spawns the full image, but the image is animated and should spawn the right frame based on the one assigned on the parent
