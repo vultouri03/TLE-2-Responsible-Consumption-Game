57334c4363490e3c1cf304c988e5080f05e7e55d
michielhijweege <michielhijweege@hotmail.nl>
2024-05-21 13:47:47 +0200

    Add the first version of the save score system

--- score_Manager.gd
+++ score_Manager.gd
@@ -4,31 +4,65 @@ class_name scoremanager
 # the current score
 var currentScore = 0
  
+var highscores = [0, 0, 0, 0, 0]
+
+# where to save
+const save_path = "user://savefile.save"
+
 # load on start
 func _ready():
-	load_score()
+	_load_score()
 
-# reset current score begin game
-func reset_score():
+# rcall this functie at begin of game to make sure it is 0
+func _reset_score():
 	currentScore = 0
 	pass
 
 # add score (score) amount to add on current score
-func add_score(score):
+func _add_score(score):
 	currentScore += score
 	pass
 
 # remove score (score) amount to remove on current score
-func remove_score(score):
+func _remove_score(score):
 	currentScore -= score
 	pass
 
+# call this functie at end of game so he check highscore
+func _endgame():
+	# Insert the current score into the highscores array if it qualifies
+	highscores.append(currentScore)
+	highscores.sort()  # Sort in ascending order
+	highscores.reverse()  # Reverse to get descending order
+	highscores = highscores.slice(0, 5)  # Keep only the top 5 scores
+	_save_score()  # Save the updated high scores
+	pass
+
 # save the highscore 
-func save_score():
+func _save_score():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	for score in highscores:
+		file.store_var(score)
 	pass
 	
  
 # load the current highscore
-func load_score():
-	
+func _load_score():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		for i in range(highscores.size()):
+			highscores[i] = file.get_var(highscores[i])
+	else:
+		print("no data saved...")
+		highscores = [0, 0, 0, 0, 0]
+	print(highscores)
+	pass
+
+# delete the current highscore
+func _delete_score():
+	if FileAccess.file_exists(save_path):	
+		DirAccess.remove_absolute(save_path)
+		_load_score()
+	else:
+		print("no data saved...")
 	pass
