6eeef67204c612bb8f9f56133a2b59add87a8f45
vultouri03 <youridegier@gmail.com>
2024-06-21 21:09:45 +0200

    fixed end screen ui

--- end_UI.gd
+++ end_UI.gd
@@ -8,14 +8,14 @@ func _ready():
 # Load the text into the game
 func _settext():
 	# TEST show current score
-	$"TESTSCORE/Current Score".text = str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/TESTSCORE/Current Score".text = str($score_Manager.currentScore)
 	
-	$"Highscores/Current Score".text = "CURRENT SCORE : " + str($score_Manager.currentScore)
-	$"Highscores/Highscore 1".text = str($score_Manager.highscores[0])
-	$"Highscores/Highscore 2".text = str($score_Manager.highscores[1])
-	$"Highscores/Highscore 3".text = str($score_Manager.highscores[2])
-	$"Highscores/Highscore 4".text = str($score_Manager.highscores[3])
-	$"Highscores/Highscore 5".text = str($score_Manager.highscores[4])
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Current Score".text = "CURRENT SCORE : " + str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Highscore 1".text = str($score_Manager.highscores[0])
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Highscore 2".text = str($score_Manager.highscores[1])
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Highscore 3".text = str($score_Manager.highscores[2])
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Highscore 4".text = str($score_Manager.highscores[3])
+	$"ParallaxBackground/ParallaxLayer2/Highscores/Highscore 5".text = str($score_Manager.highscores[4])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,12 +23,12 @@ func _process(delta):
 
 # restart the game button
 func _on_retry_button_pressed():
-	print('Restart game');
+	get_tree().change_scene_to_file("res://Levels/lvl_main_menu.tscn")
 	pass
 
 # quit the game
 func _on_quit_button_pressed():
-	print('Quit game');
+	get_tree().quit()
 	pass
 
 
@@ -41,23 +41,23 @@ func _on_tips_button_pressed():
 #THIS IS TO TEST SCORE
 func _on_add_button_pressed():
 	$score_Manager._add_score(1)
-	$"TESTSCORE/Current Score".text = str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/TESTSCORE/Current Score".text = str($score_Manager.currentScore)
 	pass # Replace with function body.
 
 func _on_remove_button_pressed():
 	$score_Manager._remove_score(1)
-	$"TESTSCORE/Current Score".text = str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/TESTSCORE/Current Score".text = str($score_Manager.currentScore)
 	pass # Replace with function body.
 
 func _on_save_button_pressed():
 	$score_Manager._endgame()
-	$"TESTSCORE/Current Score".text = str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/TESTSCORE/Current Score".text = str($score_Manager.currentScore)
 	pass # Replace with function body.
 
 func _on_load_button_pressed():
 	$score_Manager._load_score()
 	_settext()
-	$"TESTSCORE/Current Score".text = str($score_Manager.currentScore)
+	$"ParallaxBackground/ParallaxLayer2/TESTSCORE/Current Score".text = str($score_Manager.currentScore)
 	pass # Replace with function body.
 
 func _on_delete_button_pressed():
